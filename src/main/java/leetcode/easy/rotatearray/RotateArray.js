d4547894f1c4d628a433d1722fff3596c16a6a5d
ashitsathish <ashitsathish8118@gmail.com>
2020-09-28 12:01:20 -0400

    Rotate Array JS leet code easy

---
+++ src/main/java/leetcode/easy/rotatearray/RotateArray.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    swap(nums, 0, nums.length-1);
+    swap(nums,0, k-1);
+    swap(nums, k, nums.length-1);
+    console.log(nums)
+};
+
+function swap(nums,i, j) {
+    while(i<j){
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+
+rotate([1,2,3,4,5,6,7],3);
